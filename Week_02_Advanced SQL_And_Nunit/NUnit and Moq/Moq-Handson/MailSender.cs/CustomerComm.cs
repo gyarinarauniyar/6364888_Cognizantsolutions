728fecd7554f245f68a2491b82c10c453c3d8575
gyarinarauniyar <gyarinarauniyar2020@gmail.com>
2025-07-06 16:59:32 +0530

    Added Week 03 Entity Framework Core folder

---
+++ Week_02_Advanced SQL_And_Nunit/NUnit and Moq/Moq-Handson/MailSender.cs/CustomerComm.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        private readonly IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            // simulate business logic
+            string toAddress = "cust123@abc.com";
+            string message = "Some Message";
+
+            _mailSender.SendMail(toAddress, message);
+
+            return true;
+        }
+    }
+}
---
+++ Week_02_Advanced SQL_And_Nunit/NUnit and Moq/Moq-Handson/MailSender.cs/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MailSender.cs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MailSender.cs")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("59ccf807-a8f1-4f0b-a939-1ea6fa8e4057")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
